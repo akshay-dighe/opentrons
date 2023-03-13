a4bcdbd31186b232a297ab4d41fc95ef35082030
aditya-sancheti <aditya.sancheti@softdel.com>
2023-03-13 13:22:10 +0530

    Test commit

--- protocol-designer/src/components/ProtocolEditor.tsx
+++ protocol-designer/src/components/ProtocolEditor.tsx
@@ -48,6 +48,7 @@ function ProtocolEditorComponent(): JSX.Element {
             <MainPageModalPortalRoot />
             <LabwareUploadMessageModal />
             <ConnectedMainPanel />
+            
           </div>
         </div>
       </div>
