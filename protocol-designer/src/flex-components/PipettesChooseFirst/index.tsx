f803dc38ac9f35a87f24d8a6bee87bcae3d0c290
aditya-sancheti <aditya.sancheti@softdel.com>
2023-03-22 10:58:42 +0530

    Setup design for flex initial screen name and desc

---
+++ protocol-designer/src/flex-components/PipettesChooseFirst/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import cx from 'classnames'
+import flexStyles from '../FlexComponents.css'
+import styles from './PipettesChooseFirst.css'
+
+export const PipettesChooseFirstComponent = () => {
+    return <>
+        <div>
+            <div>
+                <p className={cx(flexStyles.title16_700)}>
+                    Pipettes Choose First
+                </p>
+            </div>
+        </div>
+    </>
+}
